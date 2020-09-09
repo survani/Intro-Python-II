fafad902e7fac1babe5879e5b7f98e3562c59295
survani <giovanicode@outlook.com>
2020-09-08 20:08:28 -0700

    Initial Commit

--- src/adv.py
+++ src/adv.py
@@ -48,4 +48,6 @@ room['treasure'].s_to = room['narrow']
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
-# If the user enters "q", quit the game.
+# If the user enters "q", quit the game
+
+# initial commit
